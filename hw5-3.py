3dbbffb5f7be05b32022d72628f2e91349d943e6
p22cgussen <p22cgussen@fairfieldprep.org>
2021-11-02 09:46:24 -0400

    complete hw5-3

---
+++ hw5-3.py
@@ -0,0 +1,15 @@
+# Author CCG 11/2/21
+
+import calendar
+
+# Question 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+calendar.TextCalendar(calendar.SUNDAY).pryear(2020)
+
+# Question 3
+calendar.TextCalendar().prmonth(2004, 7)
+
+# Question 5
+print(calendar.isleap(2004))
